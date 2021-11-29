f28b6bcc0b15fee8d16c6b2bd7ae0d7f206d6db6
a-tiny-flame <atinyflame@gmail.com>
2021-11-29 14:00:17 -0600

    rust: 8kyu - bool_to_word

---
+++ rust/8kyu/bool_to_word/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {}
+
+fn bool_to_word(value: bool) -> &'static str {
+    if value {
+        "Yes"
+    } else {
+        "No"
+    }
+}
+
+// fn bool_to_word(value: bool) -> &'static str {
+//     match value {
+//         true => "Yes",
+//         false => "No",
+//     }
+// }
+
+// https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
+#[cfg(test)]
+mod tests {
+    use super::bool_to_word;
+
+    #[test]
+    fn example_tests() {
+        assert_eq!(bool_to_word(true), "Yes");
+        assert_eq!(bool_to_word(false), "No");
+    }
+}
