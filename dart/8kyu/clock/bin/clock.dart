4be14745b84cf05f9f399d30bdcfcebc5ef13984
a-tiny-flame <atinyflame@gmail.com>
2021-12-02 13:06:20 -0600

    dart: 8kyu - Beginner Series #2 Clock

---
+++ dart/8kyu/clock/bin/clock.dart
@@ -0,0 +1,15 @@
+import "package:test/test.dart";
+
+void main() {
+  group("Basic tests", () {
+    test("past(0, 1, 1)", () => expect(past(0, 1, 1), equals(61000)));
+    test("past(1, 1, 1)", () => expect(past(1, 1, 1), equals(3661000)));
+    test("past(0, 0, 0)", () => expect(past(0, 0, 0), equals(0)));
+    test("past(1, 0, 1)", () => expect(past(1, 0, 1), equals(3601000)));
+    test("past(1, 0, 0)", () => expect(past(1, 0, 0), equals(3600000)));
+  });
+}
+
+int past(int h, int m, int s) {
+  return (h * 3600 + m * 60 + s) * 1000;
+}
