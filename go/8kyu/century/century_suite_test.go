31dd000a021c0b200d18322758bc67a77e3ed467
a-tiny-flame <atinyflame@gmail.com>
2021-12-06 01:02:28 -0600

    go: 8kyu - century

---
+++ go/8kyu/century/century_suite_test.go
@@ -0,0 +1,24 @@
+package kata
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestCentury(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Century Suite")
+}
+
+var _ = Describe("Basic tests", func() {
+	It("should return the correct values", func() {
+		Expect(century(int(1990))).To(Equal(20))
+		Expect(century(int(1705))).To(Equal(18))
+		Expect(century(int(1900))).To(Equal(19))
+		Expect(century(int(1601))).To(Equal(17))
+		Expect(century(int(2000))).To(Equal(20))
+		Expect(century(int(89))).To(Equal(1))
+	})
+})
