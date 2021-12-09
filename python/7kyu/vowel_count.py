390e12e036f9aa2b23c3ca5d2d2714da42583e5b
a-tiny-flame <atinyflame@gmail.com>
2021-12-08 21:51:53 -0600

    python: 7kyu - Vowel Count

---
+++ python/7kyu/vowel_count.py
@@ -0,0 +1,37 @@
+import codewars_test as test
+
+
+def get_count(sentence: str) -> str:
+    return sum(1 for x in sentence if x.lower() in "aeiou")
+
+
+# def getCount(s):
+#     return sum(c in 'aeiou' for c in s)
+
+
+@test.describe("Sample tests")
+def sample_tests():
+
+    @test.it("Should count all vowels")
+    def all_vowels():
+        test.assert_equals(get_count("aeiou"), 5,
+                           f"Incorrect answer for \"aeiou\"")
+
+    @test.it("Should not count \"y\"")
+    def only_y():
+        test.assert_equals(get_count("y"), 0, f"Incorrect answer for \"y\"")
+
+    @test.it("Should return 0 when no vowels")
+    def no_vowels():
+        test.assert_equals(get_count("bcdfghjklmnpqrstvwxz y"),
+                           0, f"Incorrect answer for \"bcdfghjklmnpqrstvwxz y\"")
+
+    @test.it("Should return 0 for empty string")
+    def no_vowels():
+        test.assert_equals(
+            get_count(""), 0, f"Incorrect answer for empty string")
+
+    @test.it("Should return 5 for \"abracadabra\"")
+    def test_abracadabra():
+        test.assert_equals(get_count("abracadabra"), 5,
+                           f"Incorrect answer for \"abracadabra\"")
