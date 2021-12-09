761378cecd4576850fe53b073d9c830142ce06b4
a-tiny-flame <atinyflame@gmail.com>
2021-12-08 18:50:36 -0600

    rust: 8kyu - Get the mean of an array

---
+++ rust/8kyu/get_average/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {}
+
+fn get_average(marks: &[f32]) -> f32 {
+    (marks.iter().sum::<f32>() / marks.len() as f32).floor()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_average() {
+        assert_eq!(get_average(&[2., 2., 2., 2.]), 2.);
+        assert_eq!(get_average(&[1., 5., 87., 45., 8., 8.]), 25.);
+        assert_eq!(get_average(&[2., 5., 13., 20., 16., 16., 10.]), 11.);
+        assert_eq!(
+            get_average(&[
+                1., 2., 15., 15., 17., 11., 12., 17., 17., 14., 13., 15., 6., 11., 8., 7.
+            ]),
+            11.
+        );
+    }
+}
