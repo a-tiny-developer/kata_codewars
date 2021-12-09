d4f03c591144cedc8d272b8683f33312a37b0d07
a-tiny-flame <atinyflame@gmail.com>
2021-12-08 22:27:28 -0600

    rust: 8kyu - Vowel Count

---
+++ rust/7kyu/vowel_count/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {}
+
+fn get_count(string: &str) -> usize {
+    string
+        .chars()
+        .filter(|&c| c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        .count()
+}
+
+// fn get_count(string: &str) -> usize {
+//     string.chars().filter(|&c| "aeiou".contains(c)).count()
+// }
+
+#[test]
+fn my_tests() {
+    assert_eq!(get_count("abracadabra"), 5);
+}
