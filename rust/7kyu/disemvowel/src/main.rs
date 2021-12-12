0de3ca360c158ee6a3c31c791b7386432ef10043
a-tiny-flame <atinyflame@gmail.com>
2021-12-12 01:52:10 -0600

    rust: 7kyu - Disemvowel Trolls

---
+++ rust/7kyu/disemvowel/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {}
+
+fn disemvowel(s: &str) -> String {
+    s.chars().filter(|&c| !"aeoiuAEIOU".contains(c)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::disemvowel;
+
+    #[test]
+    fn example_test() {
+        assert_eq!(
+            disemvowel("This website is for losers LOL!"),
+            "Ths wbst s fr lsrs LL!"
+        );
+    }
+}
