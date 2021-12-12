4251f3d0c109cf105d5c6c34eaf32f5355767572
a-tiny-flame <atinyflame@gmail.com>
2021-12-12 15:30:06 -0600

    py: 8kyu - Calculate BMI

---
+++ python/8kyu/bmi.py
@@ -0,0 +1,24 @@
+import codewars_test as test
+
+
+def bmi(weight: int, height: float) -> str:
+    bmi = weight / (height ** 2)
+    if bmi <= 18.5:
+        return "Underweight"
+    elif bmi <= 25:
+        return "Normal"
+    elif bmi <= 30:
+        return "Overweight"
+    else:
+        return "Obese"
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(bmi(50, 1.80), "Underweight")
+        test.assert_equals(bmi(80, 1.80), "Normal")
+        test.assert_equals(bmi(90, 1.80), "Overweight")
+        test.assert_equals(bmi(110, 1.80), "Obese")
+        test.assert_equals(bmi(50, 1.50), "Normal")
