3dab1dd9e1669c299890afa6d0a79f5fc24159f1
a-tiny-flame <atinyflame@gmail.com>
2021-12-12 01:06:07 -0600

    rust: 8kyu - Do I get a bonus?

---
+++ rust/8kyu/bonus_time/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn bonus_time(salary: u64, bonus: bool) -> String {
+    format!("¥{}", if bonus { salary * 10 } else { salary })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic_cases() {
+        assert_eq!(bonus_time(10000, true), "¥100000");
+        assert_eq!(bonus_time(25000, true), "¥250000");
+        assert_eq!(bonus_time(10000, false), "¥10000");
+        assert_eq!(bonus_time(60000, false), "¥60000");
+        assert_eq!(bonus_time(2, true), "¥20");
+        assert_eq!(bonus_time(78, false), "¥78");
+        assert_eq!(bonus_time(67890, true), "¥678900");
+    }
+}
