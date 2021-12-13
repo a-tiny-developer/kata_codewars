52a626e5d673077e8a7d2d1a86c4e11cf5f6bbdf
a-tiny-flame <atinyflame@gmail.com>
2021-12-12 21:01:47 -0600

    typescript: 7kyu - Mumbling

---
+++ typescript/7kyu/mumbling/mumbling.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.accum = void 0;
+function accum(s) {
+    return s.split("").map((value, index) => value.toUpperCase() + value.toLowerCase().repeat(index)).join("-");
+}
+exports.accum = accum;
+const chai_1 = require("chai");
+describe("Fixed Tests accum", function () {
+    it("Basic tests", function () {
+        chai_1.assert.strictEqual(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        chai_1.assert.strictEqual(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        chai_1.assert.strictEqual(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        chai_1.assert.strictEqual(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        chai_1.assert.strictEqual(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    });
+});
