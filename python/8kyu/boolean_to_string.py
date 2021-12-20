fea351ebc41eea5d77270eb0e2b37f2e2dd7232e
a-tiny-flame <atinyflame@gmail.com>
2021-12-19 22:09:08 -0600

    python: 8kyu - Convert a Boolean to a String

---
+++ python/8kyu/boolean_to_string.py
@@ -0,0 +1,12 @@
+import codewars_test as test
+
+
+boolean_to_string = str
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(boolean_to_string(True), "True")
+        test.assert_equals(boolean_to_string(False), "False")
