26c0188e6ef2787afa813d6073315974f01e2d19
a-tiny-flame <atinyflame@gmail.com>
2021-12-26 21:51:32 -0600

    rust: 8ku - Begginer - Reduce but Grow

---
+++ rust/8kyu/grow/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn grow(nums: Vec<i32>) -> i32 {
+    nums.iter().fold(1, |acc, x| acc * x)
+}
+
+// fn grow(array: Vec<i32>) -> i32 {
+//     array.iter().product()
+// }
+
+#[test]
+fn basic_test() {
+    assert_eq!(grow(vec![1, 2, 3]), 6);
+    assert_eq!(grow(vec![4, 1, 1, 1, 4]), 16);
+    assert_eq!(grow(vec![2, 2, 2, 2, 2, 2]), 64);
+}
