90824359cbee9e622243efd96fdc08350a7dedac
a-tiny-flame <atinyflame@gmail.com>
2022-01-03 01:41:58 -0600

    python: 6kyu - Who likes it?

---
+++ python/6kyu/likes.py
@@ -0,0 +1,35 @@
+import codewars_test as test
+
+
+def likes(names: list[int]) -> str:
+    if len(names) == 0:
+        return 'no one likes this'
+    elif len(names) == 1:
+        return f'{names[0]} likes this'
+    elif len(names) == 2:
+        return f'{names[0]} and {names[1]} like this'
+    elif len(names) == 3:
+        return f'{names[0]}, {names[1]} and {names[2]} like this'
+    else:
+        return f'{names[0]}, {names[1]} and {len(names) - 2} others like this'
+
+# * Clever
+# def likes(names):
+#     n = len(names)
+#     return {
+#         0: 'no one likes this',
+#         1: '{} likes this',
+#         2: '{} and {} like this',
+#         3: '{}, {} and {} like this',
+#         4: '{}, {} and {others} others like this'
+#     }[min(4, n)].format(*names[:3], others=n-2)
+
+@test.it('Basic tests')
+def _():
+    test.assert_equals(likes([]), 'no one likes this')
+    test.assert_equals(likes(['Peter']), 'Peter likes this')
+    test.assert_equals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+    test.assert_equals(likes(['Max', 'John', 'Mark']),
+                       'Max, John and Mark like this')
+    test.assert_equals(
+        likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
