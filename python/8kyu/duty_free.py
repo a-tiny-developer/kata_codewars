2d0b9123d8eb6fc78904ac916d58c27473c5f6f9
a-tiny-flame <atinyflame@gmail.com>
2022-01-25 00:44:12 -0600

    python: 8kyu Holiday VIII - Duty Free

--- python/8kyu/duty_free.py
+++ python/8kyu/duty_free.py
@@ -7,4 +7,3 @@ def duty_free(price: int, discount: int, holiday_cost: int) -> int:
 
 test.describe("Basic tests")
 test.assert_equals(duty_free(12, 50, 1000), 166)
-test.assert_equals(duty_free(17, 10, 500), 294)
