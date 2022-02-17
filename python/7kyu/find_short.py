fcfa68e9cc47ed1bbbf6b82670ae66b1aca2072f
a-tiny-flame <atinyflame@gmail.com>
2022-02-16 23:40:11 -0600

    python: 7kyu - Shortest Word

---
+++ python/7kyu/find_short.py
@@ -0,0 +1,22 @@
+import codewars_test as test
+
+
+def find_short(s: str) -> int:
+    return len(min(s.split(), key=len))
+
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(find_short(
+            "bitcoin take over the world maybe who knows perhaps"), 3)
+        test.assert_equals(find_short(
+            "turns out random test cases are easier than writing out basic ones"), 3)
+        test.assert_equals(find_short(
+            "lets talk about javascript the best language"), 3)
+        test.assert_equals(find_short(
+            "i want to travel the world writing code one day"), 1)
+        test.assert_equals(find_short(
+            "Lets all go on holiday somewhere very cold"), 2)
+        test.assert_equals(find_short("Let's travel abroad shall we"), 2)
